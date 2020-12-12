a2f1fadc44fd40ee320a1750c9647aca8dbd6a10
Mickaël Andrieu <mickael.andrieu@solvolabs.com>
2020-12-12 02:44:41 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# Book (Secure) Store Node.js (Express.js) REST API for learning purposes only
+
+## Installation
+
+```
+git clone https://github.com/mickaelandrieu/secure-bookstore-node-rest-api.git
+cd secure-bookstore-node-rest-api
+npm i
+```
+
+## Endpoints
+
+### Books
+
+* `/books`
+* `/books/<isbn>`
+
+### Users
+
+* `/users`
+* `/users/<mail>`
+
+### Book Store management
+
+* `/borrow/<mail>/<isbn>`: borrow a book from the book store as an user
+* `/bring-back/<mail>/<isbn>`: bring back a book to the book store as an user
+
+## Deploy on Heroku
+
+Create [an account on Heroku](https://signup.heroku.com/) (Free/Hobby tiers is enough) and install [Heroku CLI](https://devcenter.heroku.com/articles/heroku-cli).
+
+Then, execute the following instruction in the `secure-bookstore-node-rest-api` folder:
+
+```
+heroku login
+heroku create
+git push heroku master
+```
+
+And "voila", the REST API is deployed and ready to be tested.
+
+This project is licensed under the [MIT license](https://opensource.org/licenses/MIT).
---
+++ index.js
@@ -0,0 +1,190 @@
+const express = require('express');
+const moment = require('moment');
+const app = express()
+const port = process.env.PORT || 3000;
+const cors = require('cors');
+const basicAuth = require('express-basic-auth')
+
+app.use(cors());
+app.use(basicAuth({
+    users: {
+        'john': 'superSecret',
+        'elise': '12345678'
+    },
+    challenge: true,
+    realm: 'BasicHTTPAuth'
+}))
+
+app.use(express.json())
+
+/**
+ * Demo Data START
+ */
+const users = [
+    {
+        "first_name": "Doe",
+        "last_name": "John",
+        "mail": "john.doe@studi.fr",
+        "books": [ ],
+    },
+    {
+        "first_name": "Doe",
+        "last_name": "Jane",
+        "mail": "jane.doe@studi.fr",
+        "books": [ ],
+    },
+    {
+        "first_name": "Mickaël",
+        "last_name": "Andrieu",
+        "mail": "mickael.andrieu@exemple.fr",
+        "books": ["0-7975-8110-3", "0-7975-8110-4"],
+    },
+]
+
+const books = [
+    {
+        "title": "Book 1",
+        "author": "Jonathan",
+        "isbn": "0-7975-8110-3",
+        "availability": true,
+        "return_date": null
+    },
+    {
+        "title": "Book 2",
+        "author": "Elise",
+        "isbn": "0-7975-8110-4",
+        "availability": false,
+        "return_date": moment()
+    },
+    {
+        "title": "Book 3",
+        "author": "Zineb",
+        "isbn": "0-7975-8110-5",
+        "availability": false,
+        "return_date": null
+    },
+]
+
+/**
+ * Demo Data END
+ */
+
+/**
+ * Books CRUD
+ */
+app.get('/', (req, res) => {
+    res.json({'message': "Home, nothing interesting here."})
+})
+
+app.get('/books/', (req, res) => {
+    res.json(books)
+})
+
+app.get('/books/:isbn', (req, res) => {
+    const isbn = req.params.isbn
+    const book = books.find(book => book.isbn === isbn)
+
+    res.json(book)
+})
+
+app.post('/books', (req, res) => {
+    books.push(req.body)
+    res.status(200).json(books)
+})
+
+app.put('/books/:isbn', (req,res) => {
+    const isbn = req.params.isbn
+    let book = books.find(book => book.isbn === isbn)
+
+    // Book update
+    book.title = req.body.title
+    book.author = req.body.author
+    book.availability = req.body.availability
+    book.return_date = req.body.return_date
+
+    res.status(200).json(book)
+})
+
+app.delete('/books/:isbn', (req, res) => {
+    const isbn = req.params.isbn
+    const book = books.find(book => book.isbn === isbn)
+    books.splice(books.indexOf(book), 1)
+
+    res.json(books)
+})
+
+/**
+ * Users CRUD
+ */
+app.get('/users/', (req, res) => {
+    res.json(users)
+})
+
+app.get('/users/:mail', (req, res) => {
+    const mail = req.params.mail
+    const user = users.find(user => user.mail === mail)
+ 
+    res.json(user)
+})
+
+app.post('/users', (req, res) => {
+    users.push(req.body)
+    res.json(users)
+})
+
+app.put('/users/:mail', (req,res) => {
+    const mail = req.params.mail
+    const user = users.find(user => user.mail === mail)
+
+   // Update of the user
+   utilisateur.first_name = req.body.first_name
+   utilisateur.last_name = req.body.last_name
+   utilisateur.mail = req.body.mail
+   utilisateur.books = req.body.books
+
+   res.json(user)
+})
+
+app.delete('/users/:mail', (req, res) => {
+    const mail = req.params.mail
+    const user = users.find(user => user.mail === mail)
+    users.splice(users.indexOf(user), 1)
+ 
+    res.json(users)
+})
+
+/**
+ * Book Store management
+ */
+
+app.post('/borrow/:mail/:isbn', (req, res) => {
+    const mail = req.params.mail
+    const isbn = req.params.isbn
+
+    const user = users.find(user => user.mail === mail)
+    const book = books.find(book => book.isbn === isbn)
+
+    user.books.push(book.isbn)
+    book.availability = false
+    book.return_date = moment().add(14, 'days');
+
+    return res.json(book)
+})
+
+app.post('/bring-back/:mail/:isbn', (req, res) => {
+    const mail = req.params.mail
+    const isbn = req.params.isbn
+
+    const user = users.find(user => user.mail === mail)
+    const book = books.find(book => book.isbn === isbn)
+
+    user.books.splice(user.books.indexOf(book), 1)
+    book.availability = true
+    book.return_date = null;
+
+    return res.json(user)
+})
+
+app.listen(port, () => {
+  console.log('[Server] Started.')
+})
